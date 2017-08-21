084f03e16fdf3a7513ded0dd3524cd638c955607
Robby Bourne <robbybourne1@gmail.com>
2017-08-21 09:37:22 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace generics_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
